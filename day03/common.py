b8fab108e426b00d7fe74618c175e19b9ab15f9e
Chris Tapper <mail@cjtapper.com>
2023-12-03 21:23:14 +1100

    Extract common functionality

---
+++ day03/common.py
@@ -0,0 +1,92 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+from functools import cached_property
+from typing import NamedTuple
+
+NUMBER_OR_SYMBOL = re.compile(r"([0-9]+|[^.])")
+
+
+@dataclass
+class SchematicNumber:
+    value: int
+    bounding_box: Box
+
+    def is_adjacent_to(self, symbol: SchematicSymbol) -> bool:
+        return self._adjacency_box.contains(symbol.position)
+
+    @cached_property
+    def _adjacency_box(self) -> Box:
+        return Box(
+            position=Point(
+                x=self.bounding_box.position.x - 1,
+                y=self.bounding_box.position.y - 1,
+            ),
+            width=self.bounding_box.width + 2,
+            height=self.bounding_box.height + 2,
+        )
+
+
+@dataclass
+class SchematicSymbol:
+    position: Point
+
+
+@dataclass
+class Box:
+    position: Point
+    width: int
+    height: int
+
+    @property
+    def x_min(self) -> int:
+        return self.position.x
+
+    @property
+    def x_max(self) -> int:
+        return self.position.x + self.width - 1
+
+    @property
+    def y_min(self) -> int:
+        return self.position.y
+
+    @property
+    def y_max(self) -> int:
+        return self.position.y + self.height - 1
+
+    def contains(self, point: Point) -> bool:
+        return (
+            self.x_min <= point.x <= self.x_max and self.y_min <= point.y <= self.y_max
+        )
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+
+def parse_schematic(
+    schematic: str,
+) -> tuple[list[SchematicNumber], list[SchematicSymbol]]:
+    numbers = []
+    symbols = []
+    for row_index, row in enumerate(schematic.splitlines()):
+        for match in NUMBER_OR_SYMBOL.finditer(row):
+            position = Point(x=match.start(), y=row_index)
+            if match[0].isdigit():
+                numbers.append(
+                    SchematicNumber(
+                        value=int(match[0]),
+                        bounding_box=Box(
+                            position=position, width=len(match[0]), height=1
+                        ),
+                    )
+                )
+            elif is_symbol(match[0]):
+                symbols.append(SchematicSymbol(position=position))
+    return numbers, symbols
+
+
+def is_symbol(char: str) -> bool:
+    return not (char == "." or char.isdigit())
--- day03/part1.py
+++ day03/part1.py
@@ -2,78 +2,15 @@
 
 from __future__ import annotations
 
-import re
-from dataclasses import dataclass
-from functools import cached_property
-from typing import NamedTuple
-
 import pytest
 
 import support
 
-NUMBER_OR_SYMBOL = re.compile(r"([0-9]+|[^.])")
-
-
-@dataclass
-class SchematicNumber:
-    value: int
-    bounding_box: Box
-
-    def is_adjacent_to(self, symbol: SchematicSymbol) -> bool:
-        return self._adjacency_box.contains(symbol.position)
-
-    @cached_property
-    def _adjacency_box(self) -> Box:
-        return Box(
-            position=Point(
-                x=self.bounding_box.position.x - 1,
-                y=self.bounding_box.position.y - 1,
-            ),
-            width=self.bounding_box.width + 2,
-            height=self.bounding_box.height + 2,
-        )
-
-
-@dataclass
-class SchematicSymbol:
-    position: Point
-
-
-@dataclass
-class Box:
-    position: Point
-    width: int
-    height: int
-
-    @property
-    def x_min(self) -> int:
-        return self.position.x
-
-    @property
-    def x_max(self) -> int:
-        return self.position.x + self.width - 1
-
-    @property
-    def y_min(self) -> int:
-        return self.position.y
-
-    @property
-    def y_max(self) -> int:
-        return self.position.y + self.height - 1
-
-    def contains(self, point: Point) -> bool:
-        return (
-            self.x_min <= point.x <= self.x_max and self.y_min <= point.y <= self.y_max
-        )
-
-
-class Point(NamedTuple):
-    x: int
-    y: int
+from . import common
 
 
 def solve_for(input_data: str) -> int:
-    numbers, symbols = parse_schematic(input_data)
+    numbers, symbols = common.parse_schematic(input_data)
     acc = 0
 
     for number in numbers:
@@ -81,32 +18,6 @@ def solve_for(input_data: str) -> int:
             acc += number.value
 
     return acc
-
-
-def parse_schematic(
-    schematic: str,
-) -> tuple[list[SchematicNumber], list[SchematicSymbol]]:
-    numbers = []
-    symbols = []
-    for row_index, row in enumerate(schematic.splitlines()):
-        for match in NUMBER_OR_SYMBOL.finditer(row):
-            position = Point(x=match.start(), y=row_index)
-            if match[0].isdigit():
-                numbers.append(
-                    SchematicNumber(
-                        value=int(match[0]),
-                        bounding_box=Box(
-                            position=position, width=len(match[0]), height=1
-                        ),
-                    )
-                )
-            elif is_symbol(match[0]):
-                symbols.append(SchematicSymbol(position=position))
-    return numbers, symbols
-
-
-def is_symbol(char: str) -> bool:
-    return not (char == "." or char.isdigit())
 
 
 EXAMPLE_1 = """\
