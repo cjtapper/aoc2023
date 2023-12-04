ef6b51b6780c4f828d43c9d5a242e686a2a6772f
Chris Tapper <mail@cjtapper.com>
2023-12-04 16:05:15 +1100

    Init day 4

---
+++ day04/part1.py
@@ -0,0 +1,45 @@
+# FIXME: https://adventofcode.com/2023/day/x
+
+from __future__ import annotations
+
+import pytest
+
+import support
+
+
+def solve_for(input_data: str) -> int:
+    values = (parse_line(line) for line in input_data.splitlines())
+    for v in values:
+        pass
+
+    # TODO: implement solution here!
+    return 0
+
+
+def parse_line(line: str) -> str:
+    return line
+
+
+EXAMPLE_1 = """\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+EXPECTED_1 = 13
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (EXAMPLE_1, EXPECTED_1),
+    ],
+)
+def test_example(input_data, expected):
+    assert solve_for(input_data) == expected
+
+
+if __name__ == "__main__":
+    support.cli(__file__, solve_for)
