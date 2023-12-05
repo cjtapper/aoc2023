69249cf28ca5fa390dfe9c6a0133a49894b37f9b
Chris Tapper <mail@cjtapper.com>
2023-12-05 17:08:35 +1100

    Day 5 part 1

--- day05/part1.py
+++ day05/part1.py
@@ -2,28 +2,132 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import pytest
 
 import support
 
 
+@dataclass
+class Almanac:
+    target_seeds: list[int]
+    maps: dict[str, AlmanacMap]
+
+    def find_dest(self, src_category: str, src: int, dest_category: str) -> int:
+        target = src
+        while src_category != dest_category:
+            map = self.maps[src_category]
+            target = map.find_dest(target)
+            src_category = map.dest_category
+        return target
+
+
+@dataclass
+class AlmanacMap:
+    src_category: str
+    dest_category: str
+
+    entries: list[AlmanacMapEntry]
+
+    def find_dest(self, source):
+        for entry in self.entries:
+            dest = entry.find_dest(source)
+            if dest is not None:
+                return dest
+        return source
+
+
+@dataclass
+class AlmanacMapEntry:
+    dest_range_start: int
+    src_range_start: int
+    range_length: int
+
+    @property
+    def src_range(self):
+        return range(self.src_range_start, self.src_range_start + self.range_length)
+
+    def find_dest(self, source: int) -> int | None:
+        if source in self.src_range:
+            return self.dest_range_start + source - self.src_range_start
+
+        return None
+
+
 def solve_for(input_data: str) -> int:
-    values = (parse_line(line) for line in input_data.splitlines())
-    for v in values:
-        pass
+    almanac = parse_almanac(input_data)
 
-    # TODO: implement solution here!
-    return 0
+    locations = []
+    for seed in almanac.target_seeds:
+        locations.append(almanac.find_dest("seed", seed, "location"))
+
+    return min(locations)
 
 
-def parse_line(line: str) -> str:
-    return line
+def parse_almanac(s: str) -> Almanac:
+    seed_section, *maps_s = s.split("\n\n")
+    seeds = parse_seeds(seed_section)
+
+    parsed_maps = [parse_map(map) for map in maps_s]
+    maps = {map.src_category: map for map in parsed_maps}
+
+    return Almanac(seeds, maps)
+
+
+def parse_seeds(s: str) -> list[int]:
+    _, *seeds = s.split()
+    return [int(seed) for seed in seeds]
+
+
+def parse_map(s: str) -> AlmanacMap:
+    category_line, *entry_lines = s.splitlines()
+    src_category, dest_category = category_line.split()[0].split("-to-")
+    entries = [parse_entry(line) for line in entry_lines]
+    return AlmanacMap(src_category, dest_category, entries)
+
+
+def parse_entry(s: str) -> AlmanacMapEntry:
+    dest_start, src_start, range_len = (int(i) for i in s.split())
+    return AlmanacMapEntry(dest_start, src_start, range_len)
 
 
 EXAMPLE_1 = """\
+seeds: 79 14 55 13
 
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
 """
-EXPECTED_1 = 0
+EXPECTED_1 = 35
 
 
 @pytest.mark.parametrize(
