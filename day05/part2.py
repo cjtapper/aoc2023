61bc4e8db2671c0177944606750d2634c56236b6
Chris Tapper <mail@cjtapper.com>
2023-12-07 15:28:58 +1100

    Refactor part 2 to use interval tree

--- day05/part2.py
+++ day05/part2.py
@@ -6,111 +6,35 @@
 from __future__ import annotations
 
 import itertools
-from dataclasses import dataclass
+from typing import Generator
 
 import pytest
 
 import support
 
-
-@dataclass
-class Almanac:
-    target_seeds: list[int]
-    maps: dict[str, AlmanacMap]
-
-    def find_dest(self, src_category: str, src: int, dest_category: str) -> int:
-        target = src
-        while src_category != dest_category:
-            map = self.maps[src_category]
-            target = map.find_dest(target)
-            src_category = map.dest_category
-        return target
-
-    def find_dest_ranges(
-        self, src_category: str, src_range: range, dest_category: str
-    ) -> list[range]:
-        return []
-
-
-@dataclass
-class AlmanacMap:
-    src_category: str
-    dest_category: str
-
-    entries: list[AlmanacMapEntry]
-
-    def find_dest(self, source):
-        for entry in self.entries:
-            dest = entry.find_dest(source)
-            if dest is not None:
-                return dest
-        return source
-
-
-@dataclass
-class AlmanacMapEntry:
-    dest_range_start: int
-    src_range_start: int
-    range_length: int
-
-    @property
-    def src_range(self):
-        return range(self.src_range_start, self.src_range_start + self.range_length)
-
-    def find_dest(self, source: int) -> int | None:
-        if source in self.src_range:
-            return self.dest_range_start + source - self.src_range_start
-
-        return None
-
-    def find_dest_range(self, src_range: range) -> range | None:
-        src_intersection = range_intersection(src_range, self.src_range)
-        if src_intersection:
-            return None  # TODO: this is where I left off
-        return None
+from . import common
 
 
 def solve_for(input_data: str) -> int:
-    almanac = parse_almanac(input_data)
+    almanac = common.parse_almanac(input_data)
+    seed_ranges = list(parse_seed_ranges(input_data))
 
     min_location: int = float("inf")  # type: ignore[assignment]
-    for seed_range_start, range_length in itertools.batched(almanac.target_seeds, 2):
-        for seed in range(seed_range_start, seed_range_start + range_length):
-            location = almanac.find_dest(
-                src_category="seed",
-                src=seed,
-                dest_category="location",
-            )
-            min_location = min(location, min_location)
+    for seed in itertools.chain.from_iterable(seed_ranges):
+        location = almanac.find_dest(
+            src_category="seed",
+            src=seed,
+            dest_category="location",
+        )
+        min_location = min(location, min_location)
 
     return min_location
 
 
-def parse_almanac(s: str) -> Almanac:
-    seed_section, *maps_s = s.split("\n\n")
-    seeds = parse_seeds(seed_section)
-
-    parsed_maps = [parse_map(map) for map in maps_s]
-    maps = {map.src_category: map for map in parsed_maps}
-
-    return Almanac(seeds, maps)
-
-
-def parse_seeds(s: str) -> list[int]:
-    _, *seeds = s.split()
-    return [int(seed) for seed in seeds]
-
-
-def parse_map(s: str) -> AlmanacMap:
-    category_line, *entry_lines = s.splitlines()
-    src_category, dest_category = category_line.split()[0].split("-to-")
-    entries = [parse_entry(line) for line in entry_lines]
-    return AlmanacMap(src_category, dest_category, entries)
-
-
-def parse_entry(s: str) -> AlmanacMapEntry:
-    dest_start, src_start, range_len = (int(i) for i in s.split())
-    return AlmanacMapEntry(dest_start, src_start, range_len)
+def parse_seed_ranges(s: str) -> Generator[range, None, None]:
+    _, *seeds = s.splitlines()[0].split()
+    for start, range_length in itertools.batched(seeds, 2):
+        yield range(int(start), int(start) + int(range_length))
 
 
 def merge_ranges(ranges: list[range]) -> list[range]:
